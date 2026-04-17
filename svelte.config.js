82037da5e2489065e01fbac6189d0a87666008ae
sluan-jh <sluan6@jh.edu>
2026-04-17 15:23:50 -0500

    Fixed duplicated export default function issue in svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,5 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
-import adapter from '@sveltejs/adapter-netlify';
-
 export default {
-  kit: {
-    adapter: adapter()
-  }
-};
-
-export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
   preprocess: vitePreprocess(),
 }
